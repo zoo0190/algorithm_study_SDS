49ef704292ab89951611d766dd8ccf6215187b79
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2021-06-30 22:43:40 +0900

    임시저장

--- 20210701/20210701.ts
+++ 20210701/20210701.ts
@@ -1,14 +1,23 @@
 const paragraph = 'Bob hit a ball, the hit BALL flew far after it was hit.';
 const banned = ['hit'];
 
-function answer(paragraph: string, banned: string[]) {
+type ObjType = {
+  [index: string]: object;
+};
+
+function answer(paragraph: string, banned: string[]): string {
   const regexp = paragraph.toLowerCase().replace(/[\,\.]/g, '');
   const toArr = regexp.split(' ');
-  const countObj = toArr.reduce((acc, crr) => {
+  const countObj = toArr.reduce((acc, crr): object => {
     acc[crr] = ++acc[crr] || 1;
     return acc;
   }, {});
-  console.log(Object.entries(countObj));
+  const result = Object.entries(countObj)
+    .sort((a, b) => b[1] - a[1])
+    .filter(item => {
+      return item[0] !== banned[0];
+    });
+  return result[0][0];
 }
 
-answer(paragraph, banned);
+console.log(answer(paragraph, banned));
